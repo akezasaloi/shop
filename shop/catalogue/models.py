476778d57a7ebe8774c08ed00b27b17ce6a93595
Akeza Saloi <akezasaloi@gmail.com>
2025-08-24 22:38:58 +0300

    Added authentication to the create a product feature

--- shop/catalogue/models.py
+++ shop/catalogue/models.py
@@ -7,12 +7,16 @@ class Tag(models.Model):
     name = models.CharField(max_length=28)
 
 class Product(models.Model):
+    CATEGORY_CHOICES=(
+        ('Vegetable','Vegetable'),
+        ('Fruit','Fruit')
+    )
     name = models.CharField(max_length=50)
     description = models.TextField()
     price = models.DecimalField(max_digits=4, decimal_places=2)
     stock = models.PositiveIntegerField()
     image=models.URLField(max_length=500, blank=True, null=True)  
-    category = models.ForeignKey(Category, blank=True, null=True, on_delete=models.PROTECT)
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
     tags = models.ManyToManyField(Tag, blank=True)
 
     def __str__(self):
