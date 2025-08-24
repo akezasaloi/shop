a6c2bbf8175f9a9715c30371617eaf48609ca0c1
Akeza Saloi <akezasaloi@gmail.com>
2025-08-24 12:20:38 +0300

    Remove large files and update .gitignore

--- shop/catalogue/models.py
+++ shop/catalogue/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=28)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=28)
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    image=models.URLField(max_length=500, blank=True, null=True)  
+    category = models.ForeignKey(Category, blank=True, null=True, on_delete=models.PROTECT)
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    def __str__(self):
+        return f"{self.name}, price {self.price} {self.stock}"
+
+class Subscription(models.Model):
+    name = models.CharField(max_length=30)
+
+class Customer(models.Model):
+    subscription = models.OneToOneField(Subscription, on_delete=models.CASCADE)
--- shop/catalogue/views.py
+++ shop/catalogue/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .forms import ProductForm
+from .models import Product
+from cart.views import get_cart_count 
 
-# Create your views here.
+def list_products(request):
+    products = Product.objects.all()
+    cart_count = get_cart_count(request)
+    return render(request, 'catalogue/products.html', {'products': products, 'cart_count': cart_count})
+
+def product_detail(request, id):
+    product = get_object_or_404(Product, id=id)
+    cart_count = get_cart_count(request)
+    return render(request, "catalogue/product_details.html", {"product": product, "cart_count": cart_count})
+
+@login_required
+def create_product(request):
+    cart_count = get_cart_count(request)
+    if request.method == "POST":
+        form = ProductForm(request.POST, request.FILES) 
+        if form.is_valid():
+            form.save()
+            return redirect("list_products")
+    else:
+        form = ProductForm()
+    return render(request, "catalogue/create_product.html", {"form": form, "cart_count": cart_count})
